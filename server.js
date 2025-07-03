ce8b4ea382eb0d9408983ee3478126fcf309e4d3
soad24 <157276443+soad24@users.noreply.github.com>
2025-07-03 12:52:16 +0400

    Updated server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 30001;
+const PORT = process.env.PORT || 3000;
 
 // Security middleware
 app.use(helmet({
